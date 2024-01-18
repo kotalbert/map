72716f22bce6f3023e63a5e09e1bc61266f90928
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2024-01-18 21:49:28 +0100

    Add basics

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
+	fmt.Println(colors)
+
+	var emptyMap map[string]string
+	fmt.Println(emptyMap)
+
+	colors["black"] = "#000000"
+	colors["white"] = "#ffffff"
+	colors["bogus"] = "123"
+
+	fmt.Println(colors)
+	delete(colors, "bogus")
+	fmt.Println(colors)
+
+	b := colors["black"]
+	fmt.Println(b)
+
 }
